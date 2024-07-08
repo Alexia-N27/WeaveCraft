b1502b59ddb264ed43f4c6e36f7a61e42c25fa3a
Alexia-N27 <al.nicoleau27@gmail.com>
2024-07-08 10:58:30 +0200

    Modification du controller, model roles

--- API/src/controllers/roles.controller.js
+++ API/src/controllers/roles.controller.js
@@ -4,12 +4,14 @@ import Roles from "../models/Roles.js";
 const allRoles = async (req, res) => {
   try {
     const response = await Roles.getAllRoles();
-    res.json({
+    res
+    .status(200)
+    .json({
       msg: "Tout les rôles ont été récupérés avec succès.",
       response
     });
   } catch (error) {
-    res.status(500).json({ msg: "Erreur de serveur", error });
+    res.status(500).json({ msg: "Erreur serveur", error: error.message });
   }
 };
 
@@ -20,46 +22,67 @@ const rolesById = async (req, res) => {
     if (!response) {
       return res.status(404).json({ msg: "Role non trouvée" });
     }
-    res.json({ msg: "Role récupéré avec succès", response });
+    res.status(200).json({ msg: "Role récupéré avec succès", response });
   } catch (error) {
-    res.status(500).json({ msg: "Erreur de serveur", error });
+    res.status(500).json({ msg: "Erreur serveur", error: error.message });
   }
 };
 
 // Ajout d'un rôle
 const addRoles = async (req, res) => {
   try {
-    const response = await Roles.postAddRoles(req.body);
+    const { label } = req.body;
+
+    if (!label) {
+      return res.status(404).json({ msg: "Le champ 'label' est requis" });
+    }
+
+    const response = await Roles.postAddRoles(label);
+
     res.status(201).json({ msg: "Role ajouté avec succès", response });
   } catch (error) {
-    res.status(500).json({ msg: "Erreur serveur", error });
+    res.status(500).json({ msg: "Erreur serveur", error: error.message });
   }
 };
 
 // Modification d'un rôle
 const editRoles = async (req, res) => {
   try {
-    const { id } = req.params;
-    const response = await Roles.patchEditRoles(id, req.body);
+    const { label } = req.body;
+    const roleData = [label, req.params.id];
+
+    const response = await Roles.patchEditRoles(roleData);
+
+    if (response.error) {
+      return res
+        .status(500)
+        .json({
+          msg: "Erreur lors de la mise à jour du rôle",
+          error: response.error.message
+        });
+    }
+
     if(response.affectedRows === 0) {
       return res.status(404).json({ msg: "Role non trouvé" });
     }
-    res.json({ msg: "Role modifier avec succès", response });
+    res.status(200).json({ msg: "Role modifier avec succès", response });
   } catch (error) {
-    res.status(500).json({ msg: "Erreur serveur", error });
+    res.status(500).json({ msg: "Erreur serveur", error: error.message });
   }
 };
 
 // Suppression d'un rôle
 const deleteRoles = async (req, res) => {
   try {
-    const response = await Roles.deleteRolesById(req.params.id);
+    const id = req.params.id;
+    const response = await Roles.deleteRolesById(id);
+
     if (response.affectedRows === 0) {
       return res.status(404).json({ msg: "Role non trouvé"});
     }
     res.json({ msg: "Role supprimer avec succès", response });
   } catch (error) {
-    res.status(500).json({ msg: "Erreur serveur", error });
+    res.status(500).json({ msg: "Erreur serveur", error: error.message });
   }
 };
 
--- API/src/models/Roles.js
+++ API/src/models/Roles.js
@@ -1,40 +1,56 @@
-import Query from "./Query.js";
+import pool from "../config/db.js";
 
 class Roles {
   static async getAllRoles() {
-    const query = `SELECT * FROM roles`;
-    const response = await Query.run(query);
-    return response;
+    try {
+      const query = `SELECT id, label FROM roles`;
+      const response = await pool.query(query);
+      return response[0];
+    } catch (error) {
+      return { error: error.message };
+    }
   }
 
   static async getRolesById(id) {
-    const query = `SELECT roles.id, label FROM roles WHERE id = ?`;
-    const data = {id};
-    const response = await Query.runWithParams(query, data);
-    return response;
+    try {
+      const query = `SELECT roles.id, label FROM roles WHERE id = ?`;
+      const response = await pool.execute(query, [id]);
+      return response[0];
+    } catch (error) {
+      return { error: error.message };
+    }
   }
 
   static async postAddRoles(data) {
-    const query = `
-    INSERT INTO roles (label)
-    VALUES (?)
-    `;
-    const response = await Query.runWithParams(query, data);
-    return response;
+    try {
+      const query = `
+      INSERT INTO roles (label)
+      VALUES (?)
+      `;
+      const response = await pool.execute(query, [data]);
+      return response;
+    } catch (error) {
+      return { error: error.message };
+    }
   }
 
-  static async patchEditRoles(id, body) {
-    const data = {...body, id};
-    const query = `UPDATE roles SET label = ? WHERE id = ?`;
-    const response = await Query.runWithParams(query, data);
-    return response;
+  static async patchEditRoles(body) {
+    try {
+      const query = `UPDATE roles SET label = ? WHERE id = ?`;
+      const response = await pool.execute(query, body);
+      return response[0];
+    } catch (error) {
+      return { error: error.message };
+    }
   }
 
   static async deleteRolesById(id) {
-    const data = id;
-    const query = `DELETE FROM roles WHERE id = ?`;
-    const response = await Query.runWithParams(query, data);
-    return response;
+    try {
+      const response = await pool.execute(`DELETE FROM roles WHERE id = ?`, [id]);
+      return response[0];
+    } catch (error) {
+      return { error: error.message };
+    }
   }
 }
 
