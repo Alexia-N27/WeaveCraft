b7fea2eb8bef9b335d5330d356c7f12cfa90364e
Alexia-N27 <al.nicoleau27@gmail.com>
2024-07-08 17:59:02 +0200

    Modification model et controller additionalPicture

--- API/src/models/AdditionalPictures.js
+++ API/src/models/AdditionalPictures.js
@@ -1,62 +1,78 @@
-import Query from "./Query.js";
+import pool from "../config/db.js";
 
 class Pictures {
   static async getAllPictures() {
-    const query = `
-    SELECT additionalPictures.id,
-    additionalPictures.picture_src,
-    additionalPictures.alt,
-    additionalPictures.products_id,
-    products.title AS products_name
-    FROM additionalPictures
-    JOIN products ON additionalPictures.products_id = products.id
-    ORDER BY products.title
-    `;
-    const response = await Query.run(query);
-    return response;
+    try {
+      const query = `
+      SELECT additionalPictures.id,
+      additionalPictures.picture_src,
+      additionalPictures.alt,
+      additionalPictures.products_id,
+      products.title AS products_name
+      FROM additionalPictures
+      JOIN products ON additionalPictures.products_id = products.id
+      ORDER BY products.title
+      `;
+      const response = await pool.query(query);
+      return response[0];
+    } catch (error) {
+      return { error: error.message };
+    }
   }
 
   static async getPicturesById(id) {
-    const data = {id};
-    const query = `
-    SELECT additionalPictures.id,
-    additionalPictures.picture_src,
-    additionalPictures.alt,
-    additionalPictures.products_id,
-    products.title AS products_name
-    FROM additionalPictures
-    JOIN products ON additionalPictures.products_id = products.id
-    WHERE additionalPictures.id = ?
-    `;
-    const response = await Query.runWithParams(query, data);
-    return response;
+    try {
+      const query = `
+      SELECT additionalPictures.id,
+      additionalPictures.picture_src,
+      additionalPictures.alt,
+      additionalPictures.products_id,
+      products.title AS products_name
+      FROM additionalPictures
+      JOIN products ON additionalPictures.products_id = products.id
+      WHERE additionalPictures.id = ?
+      `;
+      const response = await pool.execute(query, [id]);
+      return response[0];
+    } catch (error) {
+      return { error: error.message };
+    }
   }
 
   static async postAddPictures(data) {
-    const query = `
-    INSERT INTO additionalPictures (picture_src, alt, products_id)
-    VALUES (?, ?, ?)
-    `;
-    const response = await Query.runWithParams(query, data);
-    return response;
+    try {
+      const query = `
+      INSERT INTO additionalPictures (picture_src, alt, products_id)
+      VALUES (?, ?, ?)
+      `;
+      const response = await pool.execute(query, data);
+      return response;
+    } catch (error) {
+      return { error: error.message };
+    }
   }
 
-  static async patchEditPictures(id, body) {
-    const data = {...body, id};
-    const query = `
-    UPDATE additionalPictures
-    SET picture_src = ?, alt = ?, products_id = ?
-    WHERE id = ?
-    `;
-    const response = await Query.runWithParams(query, data);
-    return response;
+  static async patchEditPictures(body) {
+    try {
+      const query = `
+      UPDATE additionalPictures
+      SET picture_src = ?, alt = ?, products_id = ?
+      WHERE id = ?
+      `;
+      const response = await pool.execute(query, body);
+      return response[0];
+    } catch (error) {
+      return { error: error.message };
+    }
   }
 
   static async deletePicturesById(id) {
-    const data = id;
-    const query = `DELETE FROM additionalPictures WHERE id = ?`;
-    const response = await Query.runWithParams(query, data);
-    return response;
+    try {
+      const response = await pool.execute(`DELETE FROM additionalPictures WHERE id = ?`, [id]);
+      return response[0];
+    } catch (error) {
+      return { error: error.message };
+    }
   }
 };
 
