e33e8b9d17c69fc7a3463b3a475cd18106be8cff
Alexia-N27 <al.nicoleau27@gmail.com>
2024-07-23 18:48:39 +0200

    Mise en place du header

--- client/src/views/partials/Header/Header.jsx
+++ client/src/views/partials/Header/Header.jsx
@@ -1,7 +1,7 @@
 import { NavLink, Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faUser, faCartShopping, faXmark, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 import useUser from "../../../hooks/UseUser";
 
@@ -9,7 +9,7 @@ import "./_header.scss";
 
 function Header() {
   const { user, setUser } = useUser();
-  const [showDropdown, setShowDropdown] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
   const navigate = useNavigate();
 
   async function handleLogout() {
@@ -29,59 +29,75 @@ function Header() {
       }
     } catch (error) {
       console.error("Erreur de connexion :", error);
-    } finally {
-      setShowDropdown(false);
     }
   }
 
-  function handleUserIconClick() {
-    if (!user) {
-      navigate("/login");
-    } else {
-      setShowDropdown(prev => !prev);
-    }
+  function toggleMenu() {
+    setShowMenu(prev => !prev);
   }
 
-  function handleMenuOptionClick() {
-    setShowDropdown(false);
+  function closeMenu() {
+    setShowMenu(false);
   }
 
   return (
     <header>
-      <NavLink to={"/"}><h1>WeaveCraft</h1></NavLink>
       <nav>
-        {!user && (
-          <Link to={"/login"}>
-            <FontAwesomeIcon
-              icon={faUser}
-              // className="user-icon"
-            />
-          </Link>
-        )}
+        <button onClick={toggleMenu}>
+          <FontAwesomeIcon icon={faBars} />
+        </button>
 
-        {user && (
-          <>
-          {/* ajout d'une className"user-menu"? et une dropdown-menu pour la seconde div ?*/}
-            <div >
-              <FontAwesomeIcon
-                icon={faUser}
-                onClick={handleUserIconClick}
-                // className="user-icon"
-              />
-              {showDropdown && (
-                <div >
-                  <NavLink to={"/profile"} onClick={handleMenuOptionClick}>
-                    Mon compte
-                  </NavLink>
-                  <button onClick={handleLogout}>
-                    Déconnexion
-                  </button>
-                </div>
-              )}
-            </div>
-          </>
-        )}
+        <h1>
+          <Link to={"/"}>WeaveCraft</Link>
+        </h1>
+
+        <div className="nav-icons">
+          <NavLink to={user ? "/profile" : "/login"}>
+            <FontAwesomeIcon icon={faUser} />
+          </NavLink>
+          <NavLink>
+            <FontAwesomeIcon icon={faCartShopping} />
+          </NavLink>
+        </div>
       </nav>
+
+      {/* Menu déroulé */}
+      {showMenu && (
+        <nav className="nav-menu">
+          <button onClick={toggleMenu} className={"btn-faxmark"}>
+						<FontAwesomeIcon icon={faXmark} />
+					</button>
+
+          <Link to={"/"} onClick={closeMenu}>Colliers</Link>
+          <Link to={"/"} onClick={closeMenu}>Boucles</Link>
+          <Link to={"/"} onClick={closeMenu}>Bracelets</Link>
+          <Link to={"/"} onClick={closeMenu}>Manchette</Link>
+
+          <br />
+
+          <Link to={"/"} onClick={closeMenu}>Contact</Link>
+
+          {/* Utilisateur connecté, affichage btn déconnexion */}
+          {user && (
+            <button onClick={() => { handleLogout(); closeMenu(); }}>
+              Déconnexion
+            </button>
+          )}
+        </nav>
+      )}
+
+      <form className="search-form">
+        <div className="search-container">
+          <FontAwesomeIcon icon={faMagnifyingGlass} className="search-icon" />
+          <input
+            type="search"
+            name=""
+            id=""
+            placeholder="Rechercher un produit"
+            aria-label="Rechercher un produit"
+          />
+        </div>
+      </form>
     </header>
   );
 }
