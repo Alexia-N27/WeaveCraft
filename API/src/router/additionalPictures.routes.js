195bc1324f236e49b89fa7f834764f140487f6fd
Alexia-N27 <al.nicoleau27@gmail.com>
2024-08-24 22:26:56 +0200

    Mise a jour additionalPictures

--- API/src/router/additionalPictures.routes.js
+++ API/src/router/additionalPictures.routes.js
@@ -12,10 +12,10 @@ router.get("/", allPictures);
 router.get("/:id", picturesById);
 
 // Ajout d'une image
-router.post("/", adminRequired, upload.single("image"), addPictures);
+router.post("/", adminRequired, upload.single("picture_src"), addPictures);
 
 // Modification d'une image
-router.patch("/:id", adminRequired, upload.single("image"), editPictures);
+router.patch("/:id", adminRequired, upload.single("picture_src"), editPictures);
 
 // Suppression d'une image
 router.delete("/:id", adminRequired, deletePictures);
